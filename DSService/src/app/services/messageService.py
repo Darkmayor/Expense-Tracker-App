0b4fe9baa546d3a846e988782813dd99425a5570
Darkmayor <sanketkalokhe123@gmail.com>
2026-01-02 08:04:20 +0530

    Implementing Data Science and AI Service

---
+++ DSService/src/app/services/messageService.py
@@ -0,0 +1,13 @@
+from utils.messageUtil import MessagesUtil
+from services.llmservice import LLMService
+
+class MessageService:
+    def __init__(self):
+        self.messageUtil = MessagesUtil()
+        self.llmService = LLMService()
+    
+    def process_message(self, message):
+        if self.messageUtil.isBankSms(message):
+            return self.llmService.runLLM(message)
+        else:
+            return None
