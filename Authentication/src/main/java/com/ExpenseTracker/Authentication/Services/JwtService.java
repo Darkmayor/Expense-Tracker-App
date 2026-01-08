411aecea3ed9e6d31be36aec03e4cc0294e22a2c
Darkmayor <sanketkalokhe123@gmail.com>
2026-01-08 19:59:01 +0530

    Updating token duration

--- Authentication/src/main/java/com/ExpenseTracker/Authentication/Services/JwtService.java
+++ Authentication/src/main/java/com/ExpenseTracker/Authentication/Services/JwtService.java
@@ -69,7 +69,7 @@ public class JwtService {
         return Jwts.builder().claims(claims)
                 .subject(username)
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + 1000 * 60))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
                 .signWith(getSigningKey())
                 .compact();
     }
